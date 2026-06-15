b46ca98bdcf65b25e8e0cbf45a41cfba7d338638
benssutton <benssutton@hotmail.com>
2026-06-15 17:01:21 +0100

    test(java): inbound body-size 413 limit

---
+++ java-webservice-template/src/test/java/com/example/template/core/BodySizeTest.java
@@ -0,0 +1,46 @@
+package com.example.template.core;
+
+import com.example.template.support.PostgresTestSupport;
+import io.micronaut.context.annotation.Property;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
+
+@MicronautTest
+@Property(name = "micronaut.server.max-request-size", value = "100")
+class BodySizeTest extends PostgresTestSupport {
+
+    @Controller("/__bodytest")
+    static class Sink {
+        @Post(consumes = MediaType.TEXT_PLAIN)
+        String accept(@Body String body) {
+            return "len=" + body.length();
+        }
+    }
+
+    @Inject
+    @Client("/")
+    HttpClient client;
+
+    @Test
+    void oversizedBodyRejectedWith413() {
+        String big = "x".repeat(500);
+        HttpClientResponseException ex = catchThrowableOfType(
+            () -> client.toBlocking().exchange(
+                HttpRequest.POST("/__bodytest", big).contentType(MediaType.TEXT_PLAIN)),
+            HttpClientResponseException.class);
+        assertThat((Object) ex.getStatus()).isEqualTo(HttpStatus.REQUEST_ENTITY_TOO_LARGE);
+    }
+}
